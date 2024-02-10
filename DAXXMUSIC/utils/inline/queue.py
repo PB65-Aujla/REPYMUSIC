dd538389b89e7cbdede270c9bd3febef8484b752
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-02-10 15:10:02 +0400

    Update queue.py

--- DAXXMUSIC/utils/inline/queue.py
+++ DAXXMUSIC/utils/inline/queue.py
@@ -67,11 +67,8 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="Sᴋɪᴘ Tʜᴇ Sᴏɴɢ—‣‣", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text="🪰", url=f"https://t.me/LIONS_OF_PUNJAB")],
     ]
     return buttons
