d29c6ab66ef2a43583ce48171bf2d682e80547be
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-02-10 14:00:27 +0400

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,8 +51,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.one.join_chat("LIONS_OF_PUNJAB")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +71,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.two.join_chat("LIONS_OF_PUNJAB")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +91,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.three.join_chat("LIONS_OF_PUNJAB")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +111,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.four.join_chat("LIONS_OF_PUNJAB")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +131,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.five.join_chat("LIONS_OF_PUNJAB")
             except:
                 pass
             assistants.append(5)
